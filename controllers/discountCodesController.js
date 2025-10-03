7a3d1e135a6f28f932e35f0d69a3bf76d3fb6b6b
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-10-03 10:34:26 +0200

    upd: discount_codes show

--- controllers/discountCodesController.js
+++ controllers/discountCodesController.js
@@ -1,6 +1,6 @@
 const connection = require("../db/db.js");
 
-// Index: returning categories
+// Index: returning discount_codes
 const index = (req, res) => {
 	const sql = "SELECT * FROM discount_codes";
 	connection.query(sql, (err, results) => {
@@ -10,20 +10,20 @@ const index = (req, res) => {
 	});
 };
 
-// Show: returning single categories
+// Show: returning single discount_codes
 const show = (req, res) => {
 	const { id } = req.params;
-	const sql = "SELECT * FROM categories WHERE category_id = ?";
+	const sql = "SELECT * FROM discount_codes WHERE code_id = ?";
 	connection.query(sql, [id], (err, results) => {
 		if (err)
 			return res.status(500).json({ error: "Query execution error: " + err });
 		if (results.length === 0)
-			return res.status(404).json({ error: "Category not found!" });
+			return res.status(404).json({ error: "Discount code not found!" });
 		res.json(results[0]);
 	});
 };
 
-// Create: adding new category
+// Create: adding new discount_codes
 const create = (req, res) => {
 	const { name, icon } = req.body;
 	const sql = "INSERT INTO categories (name, icon) VALUES (?, ?)";
@@ -34,7 +34,7 @@ const create = (req, res) => {
 	});
 };
 
-// Update: updating existing category
+// Update: updating existing discount_codes
 const update = (req, res) => {
 	const { id } = req.params;
 	const { name, icon } = req.body;
@@ -48,7 +48,7 @@ const update = (req, res) => {
 	});
 };
 
-// Destroy: delete single category
+// Destroy: delete single discount_codes
 const destroy = (req, res) => {
 	const { id } = req.params;
 	const sql = "DELETE FROM categories WHERE category_id = ?";
