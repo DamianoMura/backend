094a14df35531bdf733e0aeab3d8cef17bca9729
DamianoMura <mura.damiano14@gmail.com>
2025-10-03 10:40:31 +0200

    [test] checking differencies between connection successful and not successful (task1 : write good connection)

--- db/db.js
+++ db/db.js
@@ -3,34 +3,19 @@ const {DB_USER, DB_HOST, DB_PWD, DB_PORT, DB_NAME}= process.env;
 const mysql = require("mysql2")
 //queries used to create the db:
 
-const connection = mysql.createConnection({
-  host: DB_HOST,
-  port: DB_PORT,
-  user: DB_USER,
-  password: DB_PWD,
-  
-})
-
-const generate = require('./nerdNest_ER_db');
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log(`mysql connected to ${connection.config.host}:${connection.config.port}`);
-  connection.query(`CREATE DATABASE ${DB_NAME}`, function (err, result) {
-    if (err) {
-      connection.config.database=DB_NAME;//setting the database name 
-      console.log("connecting to database ",connection.config.database)
-      
-    }
-    else {
-      //here we have created the new db so we need to create also all tables 
-      console.log(`Database : ${DB_NAME} created`);
-      //function from nerNest_db.js
-      generate();
-      
-      
-    }
+const db_connection = ( ) => {
+  let connection = mysql.createConnection({
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PWD
+  })
+  connection.connect((err) => {
+    if(err) connection=null;
   });
-});
+  
+  return connection;
+}
 
-module.exports = connection;
+let conn=db_connection(); 
+console.log(conn)
