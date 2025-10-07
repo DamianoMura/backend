bb4d1de630c65f6bf86bc9671f211abd6f98fb98
DamianoMura <mura.damiano14@gmail.com>
2025-10-07 13:07:21 +0200

    fix: fully woring migration and seeder with updated fields

--- db/seeder/nerdNest_ER_db.js
+++ db/seeder/nerdNest_ER_db.js
@@ -1,15 +1,24 @@
 
   const databaseTableStructure = [
+    //categories table
     "CREATE TABLE categories (category_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) UNIQUE, icon VARCHAR(255));",
-    "CREATE TABLE products (product_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), brand VARCHAR(255), description TEXT, specs TEXT, price DECIMAL, stock_quantity BIGINT, image_url VARCHAR(255), category_id INT, category_name VARCHAR(255), created_at DATE NOT NULL);",
+    //products table
+    "CREATE TABLE products (product_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), brand VARCHAR(255), description TEXT, specs TEXT, price DECIMAL, stock_quantity BIGINT, image_url VARCHAR(255), category_id INT, category_name VARCHAR(255),created_at DATE NOT NULL);",
+    //discount tables
     "CREATE TABLE discounted_items (discounted_items_id INT PRIMARY KEY AUTO_INCREMENT, product_id INT, discount_value TINYINT);",
     "CREATE TABLE discount_codes (code_id INT PRIMARY KEY AUTO_INCREMENT, code VARCHAR(255) UNIQUE, discount_percent INT, valid_from DATE, valid_until DATE);",
+    //orders table
     "CREATE TABLE orders (order_id INT PRIMARY KEY AUTO_INCREMENT, customer_name VARCHAR(255), customer_email VARCHAR(255), address_street VARCHAR(255), address_street_number SMALLINT, address_city VARCHAR(255), postal_code VARCHAR(255), country VARCHAR(255), billing VARCHAR(255), order_date DATETIME,  discount_code_id INT);",
-    "CREATE TABLE order_items (order_item_id INT PRIMARY KEY AUTO_INCREMENT, order_id INT, product_id INT, name VARCHAR(255), price DECIMAL, quantity INT);",
+    //order items table
+    "CREATE TABLE order_items (order_item_id INT PRIMARY KEY AUTO_INCREMENT, order_id INT, product_id INT, name VARCHAR(255), description TEXT, specs TEXT, price DECIMAL, quantity INT);",
     "ALTER TABLE products ADD FOREIGN KEY (category_id) REFERENCES categories(category_id);",
+    //setting foreign/primary keys for discount tables
     "ALTER TABLE discounted_items ADD FOREIGN KEY (product_id) REFERENCES products(product_id);",
+    // setting foreign/primary keys for orders table  
     "ALTER TABLE orders ADD FOREIGN KEY (discount_code_id) REFERENCES discount_codes(code_id);",
+    // setting foreign/primary keys for order_items table
     "ALTER TABLE order_items ADD FOREIGN KEY (order_id) REFERENCES orders(order_id);",
+    //setting foreign/primary keys for order_items table       
     "ALTER TABLE order_items ADD FOREIGN KEY (product_id) REFERENCES products(product_id);"
   ];
 
--- db/seeder/orderItems.js
+++ db/seeder/orderItems.js
@@ -3,6 +3,8 @@ const orderItems = [
 			order_id: 1,
 			product_id: 1,
 			product_name: "Laptop Nova X1",
+			description: "Ultrabook leggero con prestazioni elevate per professionisti in movimento",
+			specs: "Intel Core i7, 16GB RAM, 512GB SSD, 14\'\' FHD, Wi-Fi 6",
 			price: 1100,
 			quantity: 1
 		},
@@ -10,6 +12,8 @@ const orderItems = [
 			order_id: 1,
 			product_id: 26,
 			product_name: "BlitzTalk Z1",
+			description: "Headset compatto per gamer in mobilità.",
+			specs: "Bluetooth, microfono integrato, autonomia 20h",
 			price: 89,
 			quantity: 1
 		},
@@ -17,6 +21,8 @@ const orderItems = [
 			order_id: 2,
 			product_id: 35,
 			product_name: "AKRacing Masters Series Pro",
+			description: "Sedia extra large progettata per lunghe sessioni di gioco.",
+			specs: "Materiale: pelle PU, supporto fino a 150kg, reclinazione 180°",
 			price: 599,
 			quantity: 1
 		},
@@ -24,6 +30,8 @@ const orderItems = [
 			order_id: 2,
 			product_id: 44,
 			product_name: "Titan Arena",
+			description: "Scrivania robusta per setup multi-schermo e accessori.",
+			specs: "180x75cm, struttura rinforzata, 3 livelli",
 			price: 549,
 			quantity: 1
 		},
@@ -31,6 +39,8 @@ const orderItems = [
 			order_id: 2,
 			product_id: 26,
 			product_name: "BlitzTalk Z1",
+			description: "Headset compatto per gamer in mobilità.",
+			specs: "Bluetooth, microfono integrato, autonomia 20h",
 			price: 89,
 			quantity: 1
 		},
@@ -38,6 +48,8 @@ const orderItems = [
 			order_id: 2,
 			product_id: 83,
 			product_name: "ASUS ROG Swift PG259QN",
+			description: "Headset compatto per gamer in mobilità.",
+			specs: "Monitor eSports da 24.5\'\' con refresh rate a 360Hz.', '24.5\'\' IPS, 1920x1080, 360Hz, 1ms, G-Sync",
 			price: 699,
 			quantity: 1
 		},
@@ -45,6 +57,8 @@ const orderItems = [
 			order_id: 2,
 			product_id: 63,
 			product_name: "NovaClick Elite",
+			description: "Mouse ergonomico con supporto palmare e scroll fluido.",
+			specs: "USB, 6 pulsanti, DPI regolabile",
 			price: 39,
 			quantity: 1
 		},
@@ -52,6 +66,8 @@ const orderItems = [
 			order_id: 3,
 			product_id: 63,
 			product_name: "NovaClick Elite",
+			description: "Mouse ergonomico con supporto palmare e scroll fluido.",
+			specs: "USB, 6 pulsanti, DPI regolabile",
 			price: 39,
 			quantity: 5
 		},
@@ -59,6 +75,8 @@ const orderItems = [
 			order_id: 4,
 			product_id: 26,
 			product_name: "BlitzTalk Z1",
+			description: "Headset compatto per gamer in mobilità.",
+			specs: "Bluetooth, microfono integrato, autonomia 20h",
 			price: 89,
 			quantity: 10
 		},
@@ -66,6 +84,8 @@ const orderItems = [
 			order_id: 4,
 			product_id: 2,
 			product_name: "Laptop Titan Pro - IronWare",
+			description: "Potenza desktop in formato portatile, ideale per gaming e rendering",
+			specs: "AMD Ryzen 9, 32GB RAM, 1TB SSD, RTX 4070, 17\'\' QHD",
 			price: 1899,
 			quantity: 5
 		},
@@ -73,6 +93,8 @@ const orderItems = [
 			order_id: 5,
 			product_id: 71,
 			product_name: "Logitech G915 TKL",
+			description: "Tastiera meccanica wireless con switch a basso profilo e RGB Lightsync.",
+			specs: "Switch GL Tactile, connessione LIGHTSPEED, layout TKL",
 			price: 229,
 			quantity: 7
 		},
@@ -80,6 +102,8 @@ const orderItems = [
 			order_id: 5,
 			product_id: 12,
 			product_name: "Galaxy S23 Ultra",
+			description: "Top di gamma con fotocamera da 200MP e S-Pen.",
+			specs: "Display 6.8\'\', 512GB, 12GB RAM, Android 13",
 			price: 1399,
 			quantity: 7
 		},
