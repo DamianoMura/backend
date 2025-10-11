e94ba1a6bab365134acb2933f2ba6f482380d258
DamianoMura <mura.damiano14@gmail.com>
2025-10-11 20:01:46 +0200

    [del] cleaning up files

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController.js");
-const {
-	listProducts,
-	filterProducts,
-} = require("../controllers/productsFilterController.js");
 
 // Route for getting all products with optional sorting
 // router.get("/", productController.allProducts);
@@ -16,8 +12,5 @@ router.delete("/:slug", productController.deleteProduct);
 
 // // Lista prodotti con paginazione e ordinamento
 router.get("/", productController.allProducts); // es: /products?page=2&limit=10
-
-// // Ricerca e filtro prodotti con paginazione e ordinamento
-// router.get("/search/page", filterProducts); // es: /products/search/page?term=scarpe&page=2&limit=10
 
 module.exports = router;
